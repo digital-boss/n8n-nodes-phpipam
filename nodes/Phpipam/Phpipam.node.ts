23b36a0e4125b383821a798f1fc19ab97483f3c3
xlincw0w <khaled.khazem@yahoo.com>
2022-08-30 15:05:17 +0100

    Png phpipam image

--- nodes/Phpipam/Phpipam.node.ts
+++ nodes/Phpipam/Phpipam.node.ts
@@ -29,7 +29,7 @@ export class Phpipam implements INodeType {
 	description: INodeTypeDescription = {
 		displayName: 'Phpipam',
 		name: 'Phpipam',
-		icon: 'file:phpipam.svg',
+		icon: 'file:phpipam.png',
 		group: ['transform'],
 		version: 1,
 		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
